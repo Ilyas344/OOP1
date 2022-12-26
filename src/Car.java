5fdbb13f02a16260968064fbf47450b140e1379d
Ilyas <xxxilyas92xxx@gmail.com>
2022-12-26 20:14:58 +0500

    ООП Часть 1. готовая

---
+++ src/Car.java
@@ -0,0 +1,109 @@
+import java.util.Objects;
+
+public class Car {
+    String brand;
+    String model;
+    Double engineVolume;
+    String color;
+    Integer year;
+    String country;
+
+    public Car(String brand, String model, Double engineVolume, String color, Integer year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Автомобиль: " + brand +
+                " " + model + ", сборка страна: " + country +
+                ", объем двигателя: " + engineVolume +
+                ", цвет кузова: " + color + ", " + year + " год выпуска.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(engineVolume, car.engineVolume) && Objects.equals(color, car.color) && Objects.equals(year, car.year) && Objects.equals(country, car.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, engineVolume, color, year, country);
+    }
+
+    public String getBrand() {
+
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        if (brand == null || brand.equals("")) {
+            brand = "default";
+        }
+        this.brand = brand;
+    }
+
+    public String getModel() {
+
+        return model;
+    }
+
+    public void setModel(String model) {
+        if (model == null || model.equals("")) {
+            model = "default";
+        }
+        this.model = model;
+    }
+
+    public Double getEngineVolume() {
+
+        return engineVolume;
+    }
+
+    public void setEngineVolume(Double engineVolume) {
+        if (engineVolume <= 0) {
+            engineVolume = 1.5;
+        }
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color == null || color.equals("")) {
+            color = "белый";
+        }
+        this.color = color;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        if (year == null) {
+            year = 2000;
+        }
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        if (country == null || country.equals("")) {
+            country = "default";
+        }
+        this.country = country;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car lada = new Car("Lada", "Granta", (1.7), "желтый", 2015, "Россия");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", (3.0), "черный", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8", (3.0), "черный", 2015, "Германия");
+        Car kia = new Car("Kia", "Sportage 4-го поколения", (2.4), "красный", 2018, "Ю.Корея");
+        Car hyundai = new Car("Hyundai", "Avante", (1.6), "оранжевый", 2016, "Ю.Корея");
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
+        System.out.println();
+        lada.setBrand(null);
+        bmw.setColor("");
+        audi.setCountry(null);
+        kia.setYear(0);
+        hyundai.setModel("");
+        bmw.setEngineVolume(0.);
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
     }
 }
